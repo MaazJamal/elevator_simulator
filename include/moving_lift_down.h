306ba23851a89b4a53a434764218ce50d96f8a2d
MaazJamal <30934341+MaazJamal@users.noreply.github.com>
2020-03-22 00:02:48 -0400

    fixed the floating input bug

--- include/moving_lift_down.h
+++ include/moving_lift_down.h
@@ -1,3 +1,7 @@
+/**@file moving_lift_down.h 
+ *@brief contains declaration for structure moving_lift_down function that changes the direction of lift.
+ the function is defination found in moving_lift_down.c 
+*/
 #ifndef __MOVING_LIFT_DOWN_H__
 #define __MOVING_LIFT_DOWN_H__
 
--- src/input_lifts.c
+++ src/input_lifts.c
@@ -15,24 +15,26 @@
  * @return int Returns -1 if errors in input and integer if input is fine. 
  *  */ 
 int input_lifts(void){
+     
     int temp;
     puts("Please Enter an integer number for the number of Elevator in building.");
     puts("Valid entries are 1-100");
     int result = scanf("%d", &temp);
     printf("This is result %d \n",result);
+    
     /**  if the scanf does not read anthing clear the stdin buffer and return -1*/
     if (result == 0){ 
         puts("Non-integer input entered. Please enter an integer.\n");
-        scanf("%*[^\n]"); //discards the inputs in stdin buffer
-        return -1;
+        temp = -1;
     }
-    if (temp < 1){
+    else if (temp < 1){
         puts("Value entered is less than 1");
-        return -1;
+        temp = -1;
     }
-    if (temp > 100){
+    else if (temp > 100){
         puts("Value entered is greater than 100");
-        return -1;
+        temp =  -1;
     }
+    scanf("%*[^\n]");  //discards the leftover inputs in stdin buffer
     return temp;
 }
