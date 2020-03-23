24a04200994259f139737a0a70b1c095939bdebf
unknown <hariprasadm1408@gmail.com>
2020-03-23 17:38:13 -0400

    Add existing file

---
+++ include/count_passenger.h
@@ -0,0 +1,6 @@
+#ifndef __COUNT_PASSENGER_H__
+#define __COUNT_PASSENGER_H__
+
+extern int count_passenger();
+
+#endif
--- src/count_passenger.c
+++ src/count_passenger.c
@@ -1,16 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include "count_passenger.h"
+/**
+ * The function opens the input file
+ *
+ * Reads line by line and counts the number of passenger based on passenger ID.
+ * When passenger ID equal to "911" it is considered as Emergency stop and wont be counted towards the passenger_count.
+ *
+ * @return int Returns the passenger count.
+ *  */
 int count_passenger()
 {
-    int passenger_count=0;
+    /**  es to store the number of emergency stop situations*/
+    int es=0;
+    passenger_count=0; //global variable stores the passenger count
     FILE *f;
     char a[10];
-    f=fopen("D:\\Software in C\\fwdinputfiles\\input.txt","r");
+    f=fopen("datafolder/input.txt","r");
     while(fscanf(f,"%s",a)!=EOF)
     {
+        if((passenger_count==0)||(passenger_count%4)==0)
+        {
+            if(atoi(a)==911)
+            {
+            es++;
+            }
+        }
         passenger_count++;
     }
     fclose(f);
-    return passenger_count/4;
+    passenger_count=passenger_count/4;
+    passenger_count=passenger_count-es;
+    return passenger_count;  //returns the passenger count
 }
+
 
