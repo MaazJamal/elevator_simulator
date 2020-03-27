2a291147a491decdcf8db4b6b02ca0d1bb300292
unknown <hariprasadm1408@gmail.com>
2020-03-27 15:42:18 -0400

    Add existing file

---
+++ include/count_passenger.h
@@ -0,0 +1,11 @@
+/**
+ * @file count_passenger.h
+ * @brief Counts the number of passengers from the input file.
+ */
+ 
+#ifndef __COUNT_PASSENGER_H__
+#define __COUNT_PASSENGER_H__
+
+extern int count_passenger();
+
+#endif
