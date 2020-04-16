1789ca1223e68175fbc6e39b1e41d709da94700f
Divyalakshmi Padmanaban <dpadm049@uottawa.ca>
2020-04-15 23:42:12 -0400

    Add existing file

---
+++ include/read_passenger.h
@@ -0,0 +1,16 @@
+  
+/**@file read passenger.h 
+ * *@brief contains declaration for structure read_passenger function.
+Contains declaration for read_passenger function -Reads the input file for passenger id,arrival time,starting floor and destination floor.
+
+Enables to read the data from file and store in a structure of maximum capacity 100.
+
+The function  defination is found in read_passeneger.c 
+*/
+#ifndef __READ_PASSENGER_H__
+#define __READ_PASSENGER_H__
+#include "structures.h"
+
+extern int read_passenger(struct passenger*p);
+
+#endif
---
+++ src/create_elevator_array.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"structure.h"
+#include"create_elevator_array.h"
+
+/**
+creates an array for elevators with respective to input entered by user.
+
+
+Constraints
+
+checks the input parameter-no of elevators .
+
+No of elevators should be in range(1-100).
+
+Prints error message if the no of elevators is out of range and if memory allocation fails.
+
+Returns the array of elevators.
+
+**/
+
+
+
+struct elevator * create_elevator_array(int no_of_elevators)
+{
+	
+	if((no_of_elevators==0)||(no_of_elevators>100)||(no_of_elevators<1))//checks if the input parameter is valid.
+	{
+		printf("Input parameter -Number of elevators entered is invalid \n");
+		return 0;
+	}
+	struct elevator *ele;
+	ele=malloc (no_of_elevators * sizeof(struct elevator));//creates memory for the input-no_of_elevators.
+	if(ele==NULL)//checks if the memory allocation is success
+	{
+		printf("Memory allocation failed \n");
+		return 0;
+	}
+	else
+	{
+		
+		int i;
+		int j;
+		int k;
+		for(i=0;i<no_of_elevators;i++)//initialising the array elements
+		{
+			ele[i].moving=0;
+			ele[i].between_floor=0;
+			ele[i].direction_up=true;
+			ele[i].direction_down=false;
+			ele[i].stop_at_floor=malloc(floor_count*sizeof(bool));
+			for(k=0;k<floor_count;k++)
+			{
+				ele[i].stop_at_floor[k]=0;
+			}
+			ele[i].passenger_count=0;
+			ele[i].max_passenger=10;
+			ele[i].cur_floor=0;
+			ele[i].timer=0;
+			ele[i].passenger_arr=malloc(ele[i].max_passenger*sizeof(struct passenger));
+			for (j=0;j<e[i].max_passeneger;j++)
+			{
+			ele[i].passenger[j]->in_elevator=0;
+			ele[i].passenger[j]->arrival_time=0;
+			ele[i].passenger[j]->arrival_floor=0;
+			ele[i].passenger[j]->dest_floor=0;
+			ele[i].passenger[j]->id=0;
+			}
+			
+		}
+	
+	return ele;
+	}
+	
+}
+
