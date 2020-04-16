9fc95a211b369665d09eab6c0514e23f6b654664
Ritika Arora <ritika92.arora@gmail.com>
2020-04-16 00:17:58 -0400

    Adding files for maximum waiting time

---
+++ include/max_waiting_time.h
@@ -0,0 +1,15 @@
+/*@file max_waiting_time.h
+ *@brief Has the declaration for the max_waiting_time function
+ *@detail The function has one input parameter: time_difference which is the difference between
+ * the time passenger arrived and the time he got into the elevator
+ *@return float returns the maximum time taken by a passenger
+ */
+
+#ifndef MAX_WAITING_TIME_H_INCLUDED
+#define MAX_WAITING_TIME_H_INCLUDED
+#include "structures.h"
+#include "globals.h"
+
+extern float max_waiting_time(float time_difference);
+
+#endif // MAX_WAITING_TIME_H_INCLUDED
---
+++ src/max_waiting_time.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "../include/globals.h"
+#include "../include/max_waiting_time.h"
+
+/*
+*@brief Calculates the maximum waiting time for the passengers
+*
+*@detail The function is to measure the maximum time the passengers took to wait to get into
+* the elevator.
+* The function compares the time given as an input with static variable max_time. If the input
+* time is greater than the max time , then that time value is stored as max time.
+*
+*@param[in] time_difference, the input parameter which is the difference between the time
+* passenger arrived and the time passenger got into the elevator
+*
+* @return float Returns the maximum waiting time.
+*/
+
+float max_waiting_time(float time_difference){
+	static float max_time;
+	if(time_difference > max_time){
+		max_time = time_difference;
+	}
+	return max_time;
+}
