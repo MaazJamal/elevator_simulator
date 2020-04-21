8e4a8adb5a39d554acfa5ef0f90ccca9ca4819eb
Ritika Arora <ritika92.arora@gmail.com>
2020-04-21 05:46:13 -0400

    Fixing comments

--- src/max_waiting_time.c
+++ src/max_waiting_time.c
@@ -3,18 +3,22 @@
 #include "../include/max_waiting_time.h"
 
 /**
-*@brief Calculates the maximum waiting time for the passengers
-*
-*@detail The function is to measure the maximum time the passengers took to wait to get into
-* the elevator.
-* The function compares the time given as an input with static variable max_time. If the input
-* time is greater than the max time , then that time value is stored as max time.
-*
-*@param[in] time_difference, the input parameter which is the difference between the time
-* passenger arrived and the time passenger got into the elevator
-*
-* @return float Returns the maximum waiting time.
-**/
+ * @file max_waiting_time.c
+ *
+ * @brief Calculates the maximum waiting time for the passengers
+ *
+ * @author Ritika Arora - ritikaarora@cmail.carleton.ca
+ *
+ * @detail The function is to measure the maximum time the passengers took to wait to get into
+ * the elevator.
+ * The function compares the time given as an input with static variable max_time. If the input
+ * time is greater than the max time , then that time value is stored as max time.
+ *
+ * @param[in] time_difference, the input parameter which is the difference between the time
+ * passenger arrived and the time passenger got into the elevator
+ *
+ * @return float Returns the maximum waiting time.
+*/
 
 float max_waiting_time(float time_difference){
 	static float max_time;
