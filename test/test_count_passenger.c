b02e02a5b92524216dfcea444e7327f59471bbb4
MaazJamal <30934341+MaazJamal@users.noreply.github.com>
2020-04-21 23:05:15 -0400

    Fixed more errors and warning in test_count_passenger.c

--- test/test_count_passenger.c
+++ test/test_count_passenger.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int count_passenger(FILE *f);
 void test_count_passenger(void)
 {
     FILE *f1, *f2, *f3, *f4, *f5, *f6, *f7, *f8, *f9, *f10;
@@ -20,14 +21,14 @@ void test_count_passenger(void)
     count = count_passenger(f1);
     if (count == -1)
     {
-        char t[100];
+        char t[200];
         sprintf(t, "1)PASS: Input: Invlaid file, Expected Output: -1, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
     }
     else
     {
-        char t[100];
+        char t[200];
         sprintf(t, "1)FAIL: Input: Invalid file, Expected Output: -1, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
@@ -36,14 +37,14 @@ void test_count_passenger(void)
     count = count_passenger(f2);
     if (count == 0)
     {
-        char t[100];
+        char t[200];
         sprintf(t, "2)PASS: Input: Empty file, Expected Output: 0, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
     }
     else
     {
-        char t[100];
+        char t[200];
         sprintf(t, "2)FAIL: Input: Empty file, Expected Output: 0, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
@@ -52,14 +53,14 @@ void test_count_passenger(void)
     count = count_passenger(f3);
     if (count == 37)
     {
-        char t[100];
+        char t[200];
         sprintf(t, "3)PASS: Input: valid file, Expected Output: 37, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
     }
     else
     {
-        char t[100];
+        char t[200];
         sprintf(t, "3)FAIL: Input: Empty file, Expected Output: 37, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
@@ -68,14 +69,14 @@ void test_count_passenger(void)
     count = count_passenger(f4);
     if (count == 44)
     {
-        char t[100];
+        char t[200];
         sprintf(t, "4)PASS: Input: Varying Passenger ID in file, Expected Output: 44, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
     }
     else
     {
-        char t[100];
+        char t[200];
         sprintf(t, "4)FAIL: Input: Varying Passenger ID in file,  Expected Output: 44, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
@@ -84,14 +85,14 @@ void test_count_passenger(void)
     count = count_passenger(f5);
     if (count == 41)
     {
-        char t[100];
+        char t[200];
         sprintf(t, "5)PASS: Input: Repeating the Passenger ID, Expected Output: 41, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
     }
     else
     {
-        char t[100];
+        char t[200];
         sprintf(t, "5)FAIL: Input: Repeating the Passenger ID,  Expected Output: 41, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
@@ -100,14 +101,14 @@ void test_count_passenger(void)
     count = count_passenger(f6);
     if (count == 34)
     {
-        char t[100];
+        char t[200];
         sprintf(t, "6)PASS: Input: Giving Character strings to Passenger ID , Expected Output: 34, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
     }
     else
     {
-        char t[100];
+        char t[200];
         sprintf(t, "6)FAIL: Input: Giving Character strings to Passenger ID ,  Expected Output: 34, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
@@ -116,14 +117,14 @@ void test_count_passenger(void)
     count = count_passenger(f7);
     if (count == 35)
     {
-        char t[100];
+        char t[200];
         sprintf(t, "7)PASS: Input: Giving Emergency stop situtation 911 to Passenger ID - Count ignores 911 , Expected Output: 35, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
     }
     else
     {
-        char t[100];
+        char t[200];
         sprintf(t, "7)FAIL: Input: Giving Emergency stop situtation 911 to Passenger ID - Count ignores 911,  Expected Output: 35, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
@@ -139,7 +140,7 @@ void test_count_passenger(void)
     }
     else
     {
-        char t[100];
+        char t[200];
         sprintf(t, "8)FAIL: Input: Giving Emergency stop situtation 911 to Timestamp - Count should include the record,  Expected Output: 36, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
@@ -148,14 +149,14 @@ void test_count_passenger(void)
     count = count_passenger(f9);
     if (count == 37)
     {
-        char t[100];
+        char t[200];
         sprintf(t, "9)PASS: Input: Giving Emergency stop situtation 911 to Start floor - Count should include the record, Expected Output: 37, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
     }
     else
     {
-        char t[100];
+        char t[200];
         sprintf(t, "9)FAIL: Input: Giving Emergency stop situtation 911 to Start floor - Count should include the record,  Expected Output: 37, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
@@ -164,14 +165,14 @@ void test_count_passenger(void)
     count = count_passenger(f10);
     if (count == 37)
     {
-        char t[100];
+        char t[200];
         sprintf(t, "10)PASS: Input: Giving Emergency stop situtation 911 to Destination floor - Count should include the record, Expected Output: 37, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
     }
     else
     {
-        char t[100];
+        char t[200];
         sprintf(t, "10)FAIL: Input: Giving Emergency stop situtation 911 to Destination floor - Count should include the record,  Expected Output: 37, Output: %d .\n", count);
         printf(t);
         fprintf(test_result, t);
@@ -180,7 +181,7 @@ void test_count_passenger(void)
     fclose(test_result);
 }
 
-static int count_passenger(char *f)
+static int count_passenger(FILE *f)
 {
     /**  es to store the number of emergency stop situation*/
     int es = 0;
