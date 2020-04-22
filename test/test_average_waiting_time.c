dff401fc5a6435e203922fbc38b4423f549ad29a
Ritika Arora <ritika92.arora@gmail.com>
2020-04-21 20:48:58 -0400

    Adding test functions

---
+++ test/test_average_waiting_time.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "../include/globals.h"
+#include "../include/average_waiting_time.h"
+
+/**
+ * @file test_average_waiting_time.c
+ *
+ * @author Ritika Arora
+ *
+ * @brief tests the  average_waiting_time function if working fine or not
+ *
+ * @detail The function is to test average_waiting_time function for given inputs, the function
+ * returns right values or not. The average_waiting_time function is called with different inputs
+ * and its output is stored in one local variable. The max_time local variable should give the
+ * maximum of all the inputs given.
+ * The function checks if the max_time stores maximum of the inputs given to the max_waiting_time
+ * function , then the test passes and prints the result on console as well as writes on a file.
+ *
+ * @param[in] No input is given to the function
+ *
+ * @return void The function returns nothing.
+*/
+
+
+void test_average_waiting_time (){
+
+	FILE* test_results = fopen("R:\\Ritz\\elevator_simulator\\test\\results\\results_average_waiting_time.txt", "w+");
+
+		float time1 = 120;
+		float time2 = 249;
+		float time3 = 300;
+		float time4 = 130;
+		float average_time;
+		average_time = average_waiting_time(time1);
+		average_time = average_waiting_time(time2);
+		average_time = average_waiting_time(time3);
+		average_time = average_waiting_time(time4);
+		if(average_time >= 0){
+			char result[100];
+	        sprintf(result,"PASS: Input: 120, 249, 300, 130, Expected Output: 199.75, Output: %f .\n",average_time);
+	        printf(result);
+	        fprintf(test_results,result);
+		}
+		else{
+			char result[100];
+	        sprintf(result,"FAIL: Input: 120, 249, 300, 130, Expected Output: 199.75, Output: %f .\n",average_time);
+	        printf(result);
+	        fprintf(test_results,result);
+		}
+}
+
+
+
--- test/test_is_lift_empty.c
+++ test/test_is_lift_empty.c
@@ -2,24 +2,39 @@
 #include <stdlib.h>
 #include "../include/globals.h"
 #include "../include/structures.h"
-//#include "../include/test_is_lift_empty.h"
 #include "../include/is_lift_empty.h"
-//#include "../src/is_lift_empty.c"
 
 /**
+ * @file test_is_lift_empty.c
+ *
+ * @author Ritika Arora
+ *
  * @brief Tests if the is_lift_empty function works fine or not
  *
- * @detail
+ * @detail The function tests with different values if the output comes correct or not.
+ * elevator_count which is a global variable has been set to a fix value
+ * The test cases are as below:
+ * - For array with passenger count equal to the maximum number of passengers of the lift,
+ * 		and index value as 0 or less than the elevator count
+ * 		the output is 0
+ * - For array with passenger count less than the maximum number of passengers of the lift,
+ * 		and index value as 1 or less than the elevator count
+ * 		the output is 1
+ * - For array with passenger count more than the maximum number of passengers of the lift,
+ * 		and index value as 2 or less than the elevator count
+ * 		the output is 0
+ * - For array with index value as 3 or greater than or equal to the elevator count
+ * 		the output is -1
+ * - For an empty array, the output is -1 and the program exits
  *
  * @param[in] The function takes no input
  *
- * @return The function returns nothing.
+ * @return void The function returns nothing.
 */
 
-//unsigned int elevator_count = 1;
 void test_is_lift_empty(){
 
-	FILE* test_results = fopen("R:\\Ritz\\elevator_simulator\\test\\results\\results_is_lift_empty.txt", "w+");
+	FILE* test_results = fopen("test/results/results_is_lift_empty.txt", "w+");
 	struct elevator elevator_array[3];
 	struct elevator *ele_arr = NULL;
 	struct elevator values = { 0 };
@@ -36,6 +51,23 @@ void test_is_lift_empty(){
 	int result;
 
 	result = is_lift_empty(elevator_array,0);
+
+	if (result == 0){
+	      char temp[100];
+	      sprintf(temp,"PASS: Input: Elevator with passengers equal to the maximum passenger,"
+	              " Expected Output: 0,"
+	              " Output: %d .\n",result);
+	      printf(temp);
+	      fprintf(test_results,temp);
+	}
+	else{
+	      char temp[100];
+	      sprintf(temp,"FAIL: Input: Elevator with passengers equal to the maximum passenger,"
+	              " Expected Output: 0,"
+	              " Output: %d .\n",result);
+	      printf(temp);
+	      fprintf(test_results,temp);
+	}
 
 
 	result = is_lift_empty(elevator_array,1);
@@ -69,7 +101,7 @@ void test_is_lift_empty(){
 	}
 	else{
         char temp[100];
-        sprintf(temp,"FAIL: Input: Elevator with passengers less than the maximum passenger,"
+        sprintf(temp,"FAIL: Input: Elevator with passengers more than the maximum passenger,"
                 " Expected Output: 0,"
                 " Output: %d .\n",result);
         printf(temp);
