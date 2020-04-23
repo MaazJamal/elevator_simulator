f2ab6284999cda80a0fccd02f49f36c1094a0747
MaazJamal <30934341+MaazJamal@users.noreply.github.com>
2020-04-23 19:10:47 -0400

    added dynamic_function.h

---
+++ include/dynamic_functions.h
@@ -0,0 +1,9 @@
+/** @file dynamic_functions.h  
+ *@brief Simply imports all the headers of the different  of dynamic function
+*/
+#ifndef __DYNAMIC_FUNCTIONS_H__
+#define __DYNAMIC_FUNCTIONS_H__
+
+#include "cli_update.h"
+
+#endif
