5230e5e069a019eb192ff7b154b840419d8a7f6a
Divyalakshmi Padmanaban <dpadm049@uottawa.ca>
2020-04-23 00:02:18 -0400

    Adding emmergency_stop_handling

---
+++ src/emmergency_stop_handling.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include"structures.h"
+#include"globals.h"
+#include"emmergency_stop_handling.h"
+void emmergency_stop_handling(struct elevator ele[],int timer)
+{
+	int i,j,k=0;
+	int count;
+	bool status=true;
+	while(status)
+	{
+	
+	for(i=0;i<elevator_count;i++)
+	{   
+		    
+		    if(ele[i].passenger_count>=1)
+		    {
+		        if((ele[i].moving==1)&&(ele[i].between_floor==1))
+		        {
+			        if(ele[i].direction_up==true)
+			        {
+				        for(j=0;j<ele[i].max_passenger;j++)
+				        {
+				            ele[i].passsenger_arr[j].dest_floor=ele[i].cur_floor+1;
+				            ele[i].moving=k;
+				            ele[i].between_floor=k;
+				            remove_passenger_floor(ele[i].passenger_arr[j]);
+			            }
+			        }
+			        if(ele[i].direction_down==true)
+			        {
+				        for(j=0;j<ele[i].max_passenger;j++)
+				        {
+				            ele[i].passsenger_arr[j].dest_floor=ele[i].cur_floor-1;
+				            ele[i].moving=k;
+				            ele[i].between_floor=k;
+				            remove_passenger_floor(ele[i].passenger_arr[j]);
+			            }
+			        }
+			
+	         	}
+	        }
+		
+    }
+		timer++;
+	for(i=0;i<elevator_count;i++)
+	{
+		if(ele[i].passenegr_count!=0)
+		{
+			status=true;
+			break;
+		}
+		if(ele[i].passenger_count=0){
+			status=false;
+		}
+		
+	}
+
+    }
+}
